af236b9b74d983faed94ec89f8feafc04a26726a
hasib bin siddique <siddique.hasib@gmail.com>
2020-06-21 09:21:49 +0600

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React, {Component} from 'react';
-import { withStyles } from "@material-ui/core/styles";
 
 import Routes from "./config/Routes";
-import {connect} from "react-redux";
-
-const styles = theme => ({
-    root: {
-        display: 'flex',
-        overflow: 'hidden',
-    },
-});
 
 class App extends Component{
     render() {
-        const { classes } = this.props
-
         return (
             <div className="App">
                 <Routes />
@@ -28,4 +17,4 @@ const mapStateToProps = state => ({
 
 })
 
-export default connect(mapStateToProps)(withStyles(styles)(App));
+export default connect(mapStateToProps)(App);
